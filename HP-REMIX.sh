1856c5e52f35de92478d461453c25521cc267a24
KING HACKER <69760913+haraprasadhota@users.noreply.github.com>
2020-09-12 10:08:22 +0530

    Update HP-REMIX.sh

--- HP-REMIX.sh
+++ HP-REMIX.sh
@@ -59,26 +59,6 @@ pkg install ncurses-utils -y
 #Install sl
 pkg install sl -y
 
-
-#update HP-REMIX
-echo -e "\e[1;34m Downloading Latest Files..."
-git clone https://github.com/haraprasadhota/HP-REMIX
-if [[ -s HP-REMIX/HP-REMIX.sh ]];then
-cd HP-REMIX
-cp -r -f * .. > temp
-cd ..
-rm -rf  HP-REMIX >> temp
-rm h.x >> temp
-rm temp
-chmod +x HP-REMIX
-fi
-echo -e "\e[1;32m HP-REMIX Will Restart Now..."
-echo -e "\e[1;32m All The Required Packages Will Be Installed..."
-echo -e "\e[1;34m Press Enter To Proceed To Restart..."
-read a6
-./HP-REMIX.sh
-termux-tts-speak the script made by king hacker
-
 clear
 echo " "
 echo " "
