230326c0e75eb69fc9f7e13a54551259023b09dc
KING HACKER <69760913+haraprasadhota@users.noreply.github.com>
2020-09-12 10:09:13 +0530

    Create update.sh

---
+++ update.sh
@@ -0,0 +1,17 @@
+#update HP-REMIX
+echo -e "\e[1;34m Downloading Latest Files..."
+git clone https://github.com/haraprasadhota/HP-REMIX
+if [[ -s HP-REMIX/HP-REMIX.sh ]];then
+cd HP-REMIX
+cp -r -f * .. > temp
+cd ..
+rm -rf  HP-REMIX >> temp
+rm update.sh >> temp
+rm temp
+chmod +x HP-REMIX
+fi
+echo -e "\e[1;32m HP-REMIX Will Restart Now..."
+echo -e "\e[1;32m All The Required Packages Will Be Installed..."
+echo -e "\e[1;34m Press Enter To Proceed To Restart..."
+read a6
+./HP-REMIX.sh
