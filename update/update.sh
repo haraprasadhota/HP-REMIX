8ca643add994e67b08949170706f384a48b8dad0
KING HACKER <69760913+haraprasadhota@users.noreply.github.com>
2020-09-12 10:54:53 +0530

    Update update.sh

--- update/update.sh
+++ update/update.sh
@@ -14,4 +14,6 @@ echo -e "\e[1;32m HP-REMIX Will Restart Now..."
 echo -e "\e[1;32m All The Required Packages Will Be Installed..."
 echo -e "\e[1;34m Press Enter To Proceed To Restart..."
 read a6
+cd
+cd HP-REMIX
 ./HP-REMIX.sh
