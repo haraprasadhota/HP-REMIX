c501665f61dc116912180c56d60a7744ca39e09a
KING HACKER <69760913+haraprasadhota@users.noreply.github.com>
2020-09-12 09:58:39 +0530

    Update login.sh

--- login.sh
+++ login.sh
@@ -1,7 +1,4 @@
 #!/data/data/com.termux/files/usr/bin/bash
-
-termux-tts-speak installing python
-
 pkg install python -y
 clear
 cp login.py $PREFIX/bin/
@@ -45,22 +42,15 @@ echo " "
 echo -e "\e[32m-+-+-+-+-+-+-+-+-+-+-+-+-+-+-+-+-+-+-+- \e[m "
 echo " "
 
-termux-tts-speak please wait
-
 echo -e "\e[34m[*]\e[m \e[33mplease wait \e[m"
 sleep 3
 
-termux-tts-speak setting
 
 echo -e "\e[34m[*]\e[m \e[33msetting up user input \e[m"
 sleep 3
 
-termux-tts-speak setting up user
-
 echo -e "\e[34m[*]\e[m \e[33mcreate a password \e[m"
 sleep 2
-
-termux-tts-speak save user input
 
 echo -e "\e[34m[*]\e[m \e[33msave user input \e[m"
 echo " "
@@ -71,12 +61,9 @@ clear
 echo
 echo
 
-termux-tts-speak enter password
 
 echo -e  "\e[32mEnter password for login: \e[m"
 read passone
-
-termux-tts-speak repeat password
 
 echo -e "\e[32mRepeat password: \e[m "
 read passtwo
